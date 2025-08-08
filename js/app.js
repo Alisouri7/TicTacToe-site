7836804b39defa834780e9f4e8a746dbf7c156b4
Ali Souri <souri7205@gmail.com>
2025-08-08 22:21:10 +0330

    add dbl click to diselect

--- js/app.js
+++ js/app.js
@@ -46,4 +46,5 @@ function selectedClassRemover() {
     shapes.forEach((item) => {
         item.classList.remove('selected')
     })
-} 
+}
+window.addEventListener('dblclick', selectedClassRemover)
