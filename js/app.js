c519c16714fd3ae7dd186ec783c7c2de5d0e3555
Ali Souri <souri7205@gmail.com>
2025-08-08 17:24:34 +0330

    select shapes in app.js

---
+++ js/app.js
@@ -0,0 +1,7 @@
+let $ = document
+let crossOne = $.querySelector('.crossone')
+let crossTwo = $.querySelector('.crosstwo')
+let crossThree = $.querySelector('.crossthree')
+let circleOne = $.querySelector('.circleone')
+let circleTwo = $.querySelector('.circletwo')
+let circleThree = $.querySelector('.circlethree')
