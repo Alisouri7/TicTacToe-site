cdf07f9936f14df18cae9efcc9fd04da2f1116ce
Ali Souri <souri7205@gmail.com>
2025-08-08 22:05:07 +0330

    edit selector

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 //elements
 let $ = document
+let shapes = $.querySelectorAll('img')
 let crossOne = $.querySelector('#crossone')
 let crossTwo = $.querySelector('#crosstwo')
 let crossThree = $.querySelector('#crossthree')
@@ -17,20 +18,38 @@ let divEight = $.getElementById('7')
 let divNine = $.getElementById('8')
 ////////////////events
 crossOne.addEventListener('click', () => {
-    crossOne.classList.toggle('selected')
+    shapes.forEach((item) => {
+        item.classList.remove('selected')
+    })
+    crossOne.classList.add('selected')
 })
 crossTwo.addEventListener('click', () => {
-    crossTwo.classList.toggle('selected')
+    shapes.forEach((item) => {
+        item.classList.remove('selected')
+    })
+    crossTwo.classList.add('selected')
 })
 crossThree.addEventListener('click', () => {
-    crossThree.classList.toggle('selected')
+    shapes.forEach((item) => {
+        item.classList.remove('selected')
+    })
+    crossThree.classList.add('selected')
 })
 circleOne.addEventListener('click', () => {
-    circleOne.classList.toggle('selected')
+    shapes.forEach((item) => {
+        item.classList.remove('selected')
+    })
+    circleOne.classList.add('selected')
 })
 circleTwo.addEventListener('click', () => {
-    circleTwo.classList.toggle('selected')
+    shapes.forEach((item) => {
+        item.classList.remove('selected')
+    })
+    circleTwo.classList.add('selected')
 })
 circleThree.addEventListener('click', () => {
-    circleThree.classList.toggle('selected')
+    shapes.forEach((item) => {
+        item.classList.remove('selected')
+    })
+    circleThree.classList.add('selected')
 })
