33c35ca8329e490a72313d336f46f3fa3ea38396
Ali Souri <souri7205@gmail.com>
2025-08-08 17:54:46 +0330

    edit events in js

--- js/app.js
+++ js/app.js
@@ -5,3 +5,22 @@ let crossThree = $.querySelector('.crossthree')
 let circleOne = $.querySelector('.circleone')
 let circleTwo = $.querySelector('.circletwo')
 let circleThree = $.querySelector('.circlethree')
+////////////////////////////////////////////////
+crossOne.addEventListener('click', () => {
+    crossOne.classList.toggle('selected')
+})
+crossTwo.addEventListener('click', () => {
+    crossTwo.classList.toggle('selected')
+})
+crossThree.addEventListener('click', () => {
+    crossThree.classList.toggle('selected')
+})
+circleOne.addEventListener('click', () => {
+    circleOne.classList.toggle('selected')
+})
+circleTwo.addEventListener('click', () => {
+    circleTwo.classList.toggle('selected')
+})
+circleThree.addEventListener('click', () => {
+    circleThree.classList.toggle('selected')
+})
